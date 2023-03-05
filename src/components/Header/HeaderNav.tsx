8f4cd65e7374d40a582908002717400106594165
marsic3 <milos.arsic@lotusflare.com>
2023-03-05 11:31:46 +0100

    css

--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -13,7 +13,11 @@ const HeaderNav: FC = () => {
   return (
     <Header
       sx={{ p: 0 }}
-      className="bg-gradient-to-l from-gray-100 from-gray-300 to-cyan-200 to-blue-100 to-blue-300 shadow-md"
+      className="bg-gradient-to-l from-white-100 from-gray-300 to-cyan-200 to-blue-200 to-gray-300 drop-shadow-lg"
+      style={{
+        boxShadow:
+          "0 2.8px 2.2px rgba(0, 0, 0, 0.034), 0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06), 0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086), 0 100px 80px rgba(0, 0, 0, 0.12)",
+      }}
     >
       <Header.Item>
         <img src={derugPfp} style={{ width: "12em", paddingLeft: "1em" }} />
