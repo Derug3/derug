96f4f357f2ae7ef58838dbaf3b684f979e55800a
marsic3 <milos.arsic@lotusflare.com>
2023-03-10 16:12:18 +0100

    done

--- src/components/CollectionLayout/LeftPane.tsx
+++ src/components/CollectionLayout/LeftPane.tsx
@@ -7,28 +7,30 @@ import { FADE_DOWN_ANIMATION_VARIANTS } from "../../utilities/constants";
 export const LeftPane: FC<{
   selectedInfo: string;
   listings: IListed[] | undefined;
-}> = ({ selectedInfo, listings }) => (
-  <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS}>
-    <motion.div
-      variants={FADE_DOWN_ANIMATION_VARIANTS}
-      className="pl-10 sticky"
-    >
-      <div className="">
-        {(selectedInfo === "description" || selectedInfo === "") && (
-          <Text id="description" as="p" sx={{ p: 2 }}>
-            text
-          </Text>
-        )}
-        {selectedInfo === "listed" && listings && (
-          <Box className="flex flex-row flex-wrap mt-5 gap-1">
-            {listings.map((list: IListed) => (
-              <Box className="flex w-20">
-                <img src={list.extra.img} className="rounded-[4px]" />
-              </Box>
-            ))}
-          </Box>
-        )}
-      </div>
+}> = ({ selectedInfo, listings }) => {
+  return (
+    <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS}>
+      <motion.div
+        variants={FADE_DOWN_ANIMATION_VARIANTS}
+        className="pl-10 sticky"
+      >
+        <div className="">
+          {(selectedInfo === "description" || selectedInfo === "") && (
+            <Text id="description" as="p" sx={{ p: 2 }}>
+              text
+            </Text>
+          )}
+          {selectedInfo === "listed" && listings && (
+            <Box className="flex flex-row flex-wrap mt-5 gap-1">
+              {listings.map((list: IListed) => (
+                <Box className="flex w-20">
+                  <img src={list.extra.img} className="rounded-[4px]" />
+                </Box>
+              ))}
+            </Box>
+          )}
+        </div>
+      </motion.div>
     </motion.div>
-  </motion.div>
-);
+  );
+};
--- src/components/CollectionLayout/Proposals.tsx
+++ src/components/CollectionLayout/Proposals.tsx
@@ -1,6 +1,13 @@
-import { Button, ProgressBar } from "@primer/react";
+import {
+  Button,
+  Box,
+  Dialog,
+  FormControl,
+  ProgressBar,
+  TextInput,
+} from "@primer/react";
 import { motion } from "framer-motion";
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 import Balancer from "react-wrap-balancer";
 import { IRequest } from "../../interface/collections.interface";
 import { FADE_DOWN_ANIMATION_VARIANTS } from "../../utilities/constants";
@@ -8,18 +15,41 @@ import { FADE_DOWN_ANIMATION_VARIANTS } from "../../utilities/constants";
 export const Proposals: FC<{
   requests?: IRequest[];
 }> = ({ requests }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentRequest, setCurrentRequest] = useState<IRequest>();
+  const returnFocusRef = useRef(null);
+
   return (
     <motion.div
       className="flex w-full flex-col"
       variants={FADE_DOWN_ANIMATION_VARIANTS}
       style={{ borderTop: "2px solid  rgba(9, 194, 246)" }}
     >
+      <Dialog
+        returnFocusRef={returnFocusRef}
+        isOpen={isOpen}
+        onDismiss={() => setIsOpen(false)}
+        sx={{
+          width: "600px",
+          filter: "drop-shadow(rgb(246, 242, 9) 0px 0px 10px)",
+        }}
+        aria-labelledby="header-id"
+      >
+        <Dialog.Header id="header-id">Derug request</Dialog.Header>
+        {currentRequest?.title}
+      </Dialog>
       <div className="w-full">
         <div className="flex flex-col place-content-evenly justify-center w-full pl-5 font-mono">
           {requests ? (
             requests.map((el, index) => (
               <div className="flex gap-3 items-center justify-center py-2">
-                <Balancer className="text-lg">
+                <Balancer
+                  className="text-lg cursor-pointer font-mono"
+                  onClick={() => {
+                    setIsOpen(true);
+                    setCurrentRequest(el);
+                  }}
+                >
                   #{index} {el.title}{" "}
                 </Balancer>
                 <ProgressBar progress={50} inline sx={{ width: "200px" }} />
