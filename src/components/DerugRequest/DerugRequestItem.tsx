7f82d24573319d6f15fba0cffd912e9400eb3e6b
goran9999 <gutovicgoran@gmail.com>
2023-04-09 22:36:11 +0200

    added basic data on derug request

--- src/components/DerugRequest/DerugRequestItem.tsx
+++ src/components/DerugRequest/DerugRequestItem.tsx
@@ -1,15 +1,7 @@
-import {
-  Button,
-  Box,
-  Dialog,
-  FormControl,
-  ProgressBar,
-  TextInput,
-  Tooltip,
-} from "@primer/react";
+import { Button, ProgressBar, TextInput, Tooltip } from "@primer/react";
 import { useWallet, WalletContextState } from "@solana/wallet-adapter-react";
 import dayjs from "dayjs";
-import { motion } from "framer-motion";
+import solanaFm from "../../assets/solanaFm.jpeg";
 import { FC, useContext, useRef, useState } from "react";
 import Balancer from "react-wrap-balancer";
 import { DerugStatus } from "../../enums/collections.enums";
@@ -22,6 +14,7 @@ import { CollectionContext } from "../../stores/collectionContext";
 import { getAllNftsFromCollection } from "../../utilities/nft-fetching";
 import { toast } from "react-hot-toast";
 import { Oval } from "react-loader-spinner";
+import { getTrimmedPublicKey } from "../../solana/helpers";
 export const DerugRequestItem: FC<{
   derugRequest: IRequest;
   index: number;
@@ -94,142 +87,146 @@ export const DerugRequestItem: FC<{
   };
 
   return (
-    <div
-      className="flex w-full items-center py-2"
-      style={{
-        padding: "10px",
-      }}
-    >
-      <div className="flex gap-3 items-center justify-start w-1/2	ml-7">
-        <Balancer
-          className="text-lg cursor-pointer text-white font-mono"
-          onClick={() => {
-            setIsOpen(true);
-            setCurrentRequest(derugRequest);
-          }}
-        >
-          <span style={{ fontSize: "1em", opacity: 0.7 }}>#{index + 1}</span>{" "}
-          {""}{" "}
-        </Balancer>
-        <Balancer
-          className="text-md cursor-pointer text-white font-mono truncate w-1/3"
-          onClick={() => {
-            setIsOpen(true);
-            setCurrentRequest(derugRequest);
-          }}
-        >
-          <span style={{ fontSize: "1em", opacity: 0.7 }}>
-            {derugRequest.derugger.toString()}
-          </span>{" "}
-          {""}{" "}
-        </Balancer>
-        {derugRequest.utility &&
-          derugRequest.utility.map((u, i) => (
-            <Tooltip
+    <div className="flex flex-col w-full items-start py-4 gap-5 border-[1px] border-main-blue px-8 ">
+      {derugRequest.userData && (
+        <div className="flex items-center justify-center border-[1px] py-1 px-3">
+          <p>Doxxed</p>
+        </div>
+      )}
+      <div className="flex flex-row w-full">
+        <div className="flex gap-3 items-center justify-start w-1/2	">
+          <div className="flex flex-col items-start gap-4">
+            <div className="text-md text-white font-mono truncate flex items-center gap-4">
+              <span style={{ fontSize: "1em", opacity: 0.7 }}>
+                {getTrimmedPublicKey(derugRequest.derugger.toString())}
+              </span>
+              <img
+                src={solanaFm}
+                alt=""
+                className="rounded-[50px] w-5 cursor-pointer "
+              />
+            </div>
+            {derugRequest.userData && (
+              <div className="flex gap-5 items-center cursor-pointer hover:text-red-200">
+                <p className="text-lg">
+                  Derugger: {derugRequest.userData.twitterHandle}
+                </p>
+                <img
+                  src={derugRequest.userData.image}
+                  alt=""
+                  className="rounded-[50px] w-6"
+                />
+              </div>
+            )}
+          </div>
+          {derugRequest.utility &&
+            derugRequest.utility.map((u, i) => (
+              <Tooltip
+                sx={{
+                  "::after": {
+                    fontSize: "1em",
+                    backgroundColor: "#282C34",
+                  },
+                }}
+                direction="n"
+                aria-label={u.description}
+                noDelay={true}
+              >
+                <div
+                  className="text-sm font-mono cursor-hderugRequestp"
+                  style={{
+                    borderRightWidth:
+                      i !== derugRequest.utility.length - 1 ? "1px" : "0px",
+                    paddingRight:
+                      i !== derugRequest.utility.length - 1 ? "1em" : "0px",
+                    color: "rgb(9, 194, 246)",
+                  }}
+                >
+                  {" "}
+                  {u.title}
+                </div>
+              </Tooltip>
+            ))}
+        </div>
+        <div className="flex items-center justify-end w-1/2">
+          {showVoteButton() && (
+            <Button
+              variant="invisible"
+              sx={{ color: "rgba(9,194,246)" }}
+              onClick={castVote}
+            >
+              {loading ? (
+                <Oval color="rgb(9, 194, 246)" height={"2em"} />
+              ) : (
+                <p>Vote</p>
+              )}
+            </Button>
+          )}
+
+          {showClaimButton() && (
+            <Button variant="invisible" sx={{ color: "rgba(9,194,246)" }}>
+              Claim victory
+            </Button>
+          )}
+          {showRemintButton() && (
+            <Button variant="invisible" sx={{ color: "rgba(9,194,246)" }}>
+              Remint
+            </Button>
+          )}
+
+          <div className="relative">
+            {collectionDerug && (
+              <div
+                className={`absolute w-[1px] h-[100%] bg-red-500`}
+                style={{
+                  left: `${
+                    (collectionDerug.thresholdDenominator /
+                      collectionDerug.totalSupply) *
+                    100
+                  }%`,
+                }}
+              />
+            )}
+            <ProgressBar
+              progress={
+                (derugRequest.voteCount / (collectionDerug?.totalSupply ?? 1)) *
+                100
+              }
+              bg="#2DD4BF"
               sx={{
-                "::after": {
-                  fontSize: "1em",
-                  backgroundColor: "#282C34",
+                width: "380px",
+                height: "12px",
+                borderRadius: 0,
+                color: "rgb(45, 212, 191)",
+                "@media (max-width: 768px)": {
+                  width: "200px",
                 },
               }}
-              direction="n"
-              aria-label={u.description}
-              noDelay={true}
-            >
-              <div
-                className="text-sm font-mono cursor-hderugRequestp"
-                style={{
-                  borderRightWidth:
-                    i !== derugRequest.utility.length - 1 ? "1px" : "0px",
-                  paddingRight:
-                    i !== derugRequest.utility.length - 1 ? "1em" : "0px",
-                  color: "rgb(9, 194, 246)",
-                }}
-              >
-                {" "}
-                {u.title}
-              </div>
-            </Tooltip>
-          ))}
-      </div>
-      <div className="flex items-center justify-end w-1/2">
-        {showVoteButton() && (
-          <Button
-            variant="invisible"
-            sx={{ color: "rgba(9,194,246)" }}
-            onClick={castVote}
-          >
-            {loading ? (
-              <Oval color="rgb(9, 194, 246)" height={"2em"} />
-            ) : (
-              <p>Vote</p>
-            )}
-          </Button>
-        )}
-
-        {showClaimButton() && (
-          <Button variant="invisible" sx={{ color: "rgba(9,194,246)" }}>
-            Claim victory
-          </Button>
-        )}
-        {showRemintButton() && (
-          <Button variant="invisible" sx={{ color: "rgba(9,194,246)" }}>
-            Remint
-          </Button>
-        )}
-
-        <div className="relative">
-          {collectionDerug && (
-            <div
-              className={`absolute w-[1px] h-[100%] bg-red-500`}
-              style={{
-                left: `${
-                  (collectionDerug.thresholdDenominator /
-                    collectionDerug.totalSupply) *
-                  100
-                }%`,
-              }}
             />
-          )}
-          <ProgressBar
-            progress={
-              (derugRequest.voteCount / (collectionDerug?.totalSupply ?? 1)) *
-              100
-            }
-            bg="#2DD4BF"
-            sx={{
-              width: "380px",
-              height: "12px",
-              borderRadius: 0,
-              color: "rgb(45, 212, 191)",
-              "@media (max-width: 768px)": {
-                width: "200px",
-              },
-            }}
-          />
-        </div>
-        <Balancer className="text-lg cursor-pointer text-white font-mono px-5">
+          </div>
+          <Balancer className="text-lg cursor-pointer text-white font-mono px-5">
+            <span
+              style={{
+                padding: "10px",
+                fontSize: "0.75em",
+              }}
+            >
+              {dayjs
+                .unix(derugRequest.createdAt)
+                .toDate()
+                .toString()
+                .slice(0, 10)}
+            </span>
+          </Balancer>
           <span
+            className="text-white font-mono"
             style={{
-              padding: "10px",
               fontSize: "0.75em",
             }}
           >
-            {dayjs
-              .unix(derugRequest.createdAt)
-              .toDate()
-              .toString()
-              .slice(0, 10)}
+            {derugRequest.voteCount} / {collectionDerug?.totalSupply}
           </span>
-        </Balancer>
-        <span
-          className="text-white font-mono"
-          style={{
-            fontSize: "0.75em",
-          }}
-        >
-          {derugRequest.voteCount} / {collectionDerug?.totalSupply}
-        </span>
+        </div>
       </div>
     </div>
   );
